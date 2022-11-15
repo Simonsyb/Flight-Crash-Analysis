836df81822b09adcf83687fcf05b7b995d2902b8
Simonsyb <48290976+Simonsyb@users.noreply.github.com>
2022-11-15 00:22:17 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,18 +1,18 @@
 def crash_times(df):
-    crashTime_df = pd.DataFrame(columns=['Morning','Afternoon','Evening','Night'])
+    crashTime_df = pd.DataFrame(columns=['Morning','Afternoon','Evening','Night','Faulty Data'])
     crashTime_count = []
     time_column_idx = df.columns.get_loc("Time")
     morning_crash = 0
     afternoon_crash = 0
     evening_crash = 0
     night_crash = 0
-    count = 0
+    bad_data = 0
 
     for i in range(df.shape[0]):
         if pd.isnull(df.iat[i,time_column_idx]):
             pass
         elif int(df.iat[i,time_column_idx][:2]) >= 24:
-            pass
+            bad_data = bad_data +1
         elif 0 <= int(df.iat[i,time_column_idx][:-3]) and int(df.iat[i,time_column_idx][:-3]) <= 5:
             night_crash = night_crash +1
         elif 6 <= int(df.iat[i,time_column_idx][:-3]) and int(df.iat[i,time_column_idx][:-3]) <= 11:
@@ -21,7 +21,7 @@ def crash_times(df):
             afternoon_crash = afternoon_crash +1
         elif 18 <= int(df.iat[i,time_column_idx][:-3]) and int(df.iat[i,time_column_idx][:-3]) <= 23:
             evening_crash = evening_crash +1
-    print(count)
-    crashTime_count.extend([morning_crash,afternoon_crash,evening_crash,night_crash])        
+
+    crashTime_count.extend([morning_crash,afternoon_crash,evening_crash,night_crash,bad_data])        
     crashTime_df.loc[len(crashTime_df.index)] = crashTime_count
     return crashTime_df
